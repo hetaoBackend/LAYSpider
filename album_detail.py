f5ecde1eef1ac2165fca9b03e55a0230832bd03f
hetao <he_tao@shannonai.com>
2018-12-27 01:17:01 +0800

    FEA: add all interface

--- album_detail.py
+++ album_detail.py
@@ -2,8 +2,8 @@
 import requests
 import json
 
-#获取专辑id
-def getAlbumID(album_mid):
+# 获取专辑id和封面信息
+def getAlbumInfo(album_mid):
     url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_album_info_cp.fcg'
     header = {
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
@@ -15,7 +15,7 @@ def getAlbumID(album_mid):
         'jsonpCallback':'jsonp1',
         'loginUin':'0',
         'hostUin':'0',
-        'format':'jsonp',
+        'format':'json',
         'inCharset':'utf8',
         'outCharset':'utf-8',
         'notice':'0',
@@ -24,19 +24,14 @@ def getAlbumID(album_mid):
     }
 
     html = requests.get(url=url,params=paramters,headers=header)
-    res = json.loads(html.text.strip().lstrip('jsonp1(').rstrip(')'))['data']
-    if 'id' in res:
-        album_id = res['id']
-        album_info = getAlbumInfo(album_mid, album_id)
-        album_acturl = getActURL(album_mid, album_id)
-        actid = album_acturl['actid']
-        album_rank = getAlbumRank(album_mid,actid, 0,24)
-        print(album_rank)
+    res = json.loads(html.text.strip().lstrip('jsonp1(').rstrip(')'))
+    if 'data' in res:
+        return res['data']
     else:
-        return
+        return {}
 
-#获取专辑信息
-def getAlbumInfo(album_mid, album_id):
+# 获取专辑额外信息
+def getAlbumExtra(album_mid, album_id):
     url = 'https://c.y.qq.com/v8/fcg-bin/musicmall.fcg'
     header = {
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
@@ -50,7 +45,7 @@ def getAlbumInfo(album_mid, album_id):
         'jsonpCallback':'jsonp1',
         'loginUin':'0',
         'hostUin':'0',
-        'format':'jsonp',
+        'format':'json',
         'inCharset':'utf8',
         'outCharset':'utf-8',
         'notice':'0',
@@ -65,40 +60,7 @@ def getAlbumInfo(album_mid, album_id):
     else:
         return {}
 
-
-def getAlbumRank(album_mid, actid, begin, end):
-    url = 'https://c.y.qq.com/shop/fcgi-bin/fcg_album_rank'
-    header = {
-        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
-        'referer':'https://y.qq.com/n/yqq/album/{}.html'.format(album_mid)
-    }
-    rankname = "uin_rank_peract_{0}|uin_rank_peract_{0}_day".format(actid)
-    print(rankname)
-    paramters = {
-        'begin':'{}'.format(begin),
-        'end':'{}'.format(end),
-        'rankname':rankname,
-        'g_tk':'5381',
-        'p':'0.8575604667634174',
-        'jsonpCallback':'jsonp1',
-        'loginUin':'0',
-        'hostUin':'0',
-        'format':'jsonp',
-        'inCharset':'utf8',
-        'outCharset':'utf-8',
-        'notice':'0',
-        'platform':'yqq.json',
-        'needNewCode':'0'
-    }
-    print(paramters)
-
-    html = requests.get(url=url,params=paramters,headers=header)
-    res = json.loads(html.text.strip().lstrip('jsonp1(').rstrip(')'))
-    if 'data' in res:
-        return res['data']
-    else:
-        return {}
-
+# 获得actid，用于获得用户贡献列表
 def getActURL(album_mid, album_id):
     url = 'https://c.y.qq.com/shop/fcgi-bin/fcg_album_get_acturl'
     header = {
@@ -112,7 +74,7 @@ def getActURL(album_mid, album_id):
         'jsonpCallback':'jsonp1',
         'loginUin':'0',
         'hostUin':'0',
-        'format':'jsonp',
+        'format':'json',
         'inCharset':'utf8',
         'outCharset':'utf-8',
         'notice':'0',
@@ -127,6 +89,89 @@ def getActURL(album_mid, album_id):
     else:
         return {}
 
+# 获得用户排行榜，通过begin、end来进行分页，一页25个
+def getAlbumRank(album_mid, actid, begin, end):
+    url = 'https://c.y.qq.com/shop/fcgi-bin/fcg_album_rank'
+    header = {
+        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+        'referer':'https://y.qq.com/n/yqq/album/{}.html'.format(album_mid)
+    }
+    rankname = "uin_rank_peract_{0}|uin_rank_peract_{0}_day".format(actid)
+    paramters = {
+        'begin':'{}'.format(begin),
+        'end':'{}'.format(end),
+        'rankname':rankname,
+        'g_tk':'5381',
+        'p':'0.8575604667634174',
+        'jsonpCallback':'jsonp1',
+        'loginUin':'0',
+        'hostUin':'0',
+        'format':'json',
+        'inCharset':'utf8',
+        'outCharset':'utf-8',
+        'notice':'0',
+        'platform':'yqq.json',
+        'needNewCode':'0'
+    }
+
+    html = requests.get(url=url,params=paramters,headers=header)
+    res = json.loads(html.text.strip().lstrip('jsonp1(').rstrip(')'))
+    if 'data' in res:
+        return res['data']
+    else:
+        return {}
+
+# 获取评论列表信息
+def getCommentInfo(album_mid, top_id):
+    url = 'https://c.y.qq.com/base/fcgi-bin/fcg_global_comment_h5.fcg'
+    header = {
+        'user-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+        'referer':'https://y.qq.com/n/yqq/album/{}.html'.format(album_mid)
+    }
+    paramters = {
+        'g_tk':'5381',
+        'topid':'{}'.format(top_id), #传入top_id
+        'cmd':'8',
+        'cid':'205360772',
+        'reqtype':'2',
+        'jsonpCallback':'jsonp1',
+        'biztype':'2',
+        'domain':'qq.com',
+        'ct':'24',
+        'cv':'101010',
+        'loginUin':'0',
+        'hostUin':'0',
+        'format':'json',
+        'inCharset':'utf8',
+        'outCharset':'GB2312',
+        'needmusiccrit':'0',
+        'pagenum':'0',
+        'pagesize':'25',
+        'notice':'0',
+        'platform':'yqq.json',
+        'lasthotcommentid':'',
+        'needNewCode':'0'
+    }
+    html = requests.get(url=url,params=paramters,headers=header)
+    res = json.loads(html.text.strip())
+    return res
 
 if __name__ == "__main__":
-    getAlbumID("001T7G8z0o9FlF")
+    # 从控制台输入album_url
+    album_url = input("album_url:")
+    album_mid = album_url.lstrip('https://y.qq.com/n/yqq/album/').rstrip('.html')
+    #album_mid = "004KHjpn0p4Az7"  # album的url后缀
+    album_meta = getAlbumInfo(album_mid)
+    if 'id' in album_meta:
+        album_id = album_meta['id']
+        album_info = getAlbumExtra(album_mid, album_id)
+        album_acturl = getActURL(album_mid, album_id)
+        actid = album_acturl['actid']
+        album_rank = getAlbumRank(album_mid,actid, 0,24)
+        album_comment_info = getCommentInfo(album_mid, album_id)
+        if 'comment' in album_comment_info:
+            comment_total = album_comment_info['comment']['commenttotal']
+        if 'hot_comment' in album_comment_info:
+            hot_comment_total = album_comment_info['hot_comment']['commenttotal']
+        print(comment_total)
+        print(hot_comment_total)
